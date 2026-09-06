728ba15710ea49d488fd654b292f02e30fe9541b
Kelvin-Justin-Gordon <2018117973@ub.edu.bz>
2026-09-06 17:40:45 -0600

    updated routes in routes.go

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,13 +2,13 @@ package main
 
 import "net/http"
 
-
-
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 	mux.HandleFunc("POST /v1/consumers", app.createConsumersHandler)
 	mux.HandleFunc("POST /v1/reports", app.createReportHandler)
 	mux.HandleFunc("GET /v1/jobs/{id}", app.getJobHandler)
+	mux.HandleFunc("POST /v1/images", app.createImageHandler)          //ImageLab
+	mux.Handle("/", http.FileServer(http.Dir(app.config.frontendDir))) //Serves the frontend from the same origin as the API
 	return mux
 }
